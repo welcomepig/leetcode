b1f960d404e9a0ad68cdc77f8978aa5e5883b006
Tina Chang <welcomepig@gmail.com>
2017-08-04 22:32:53 +0800

    Updated 102

---
+++ 102-Binary-Tree-Level-Order-Traversal/102_Binary_Tree_Level_Order_Traversal2.m
@@ -0,0 +1,92 @@
+@interface TNode : NSObject
+
+@property (nonatomic, assign) NSInteger value;
+@property (nonatomic, strong) TNode *left;
+@property (nonatomic, strong) TNode *right;
+
+- (instancetype)initWithValue:(NSInteger)value;
+- (void)insert:(NSInteger)value;
+- (NSArray *)levelOrderTraversal;
+
+@end
+
+@implementation TNode
+
+- (NSString*)description {
+    /* inorder */
+    NSString *ldesc = self.left ? [NSString stringWithFormat:@"%@", self.left] : @"";
+    NSString *rdesc = self.right ? [NSString stringWithFormat:@"%@", self.right] : @"";
+    return [NSString stringWithFormat:@"%@ %ld %@", ldesc, (long)self.value, rdesc];
+}
+
+- (instancetype)initWithValue:(NSInteger)value {
+    self = [super init];
+    if (self) {
+        _value = value;
+        _left = nil;
+        _right = nil;
+    }
+    return self;
+}
+
+- (void)insert:(NSInteger)value {
+    if (value < self.value) {
+        if (!self.left) {
+            self.left = [[TNode alloc] initWithValue:value];
+        } else {
+            [self.left insert:value];
+        }
+    } else {
+        if (!self.right) {
+            self.right = [[TNode alloc] initWithValue:value];
+        } else {
+            [self.right insert:value];
+        }
+    }
+}
+
+- (NSArray *)levelOrderTraversal {
+    NSMutableArray *result = [NSMutableArray array];
+    NSMutableArray<TNode*> *queue = [NSMutableArray array];
+
+    [queue addObject:self];
+    NSUInteger count = queue.count;
+    
+    while (count != 0) {
+        NSMutableArray *nodes = [NSMutableArray array];
+        
+        for (NSUInteger i = 0;i < count; i++) {
+            TNode *node = queue[i];
+            
+            [nodes addObject:@(node.value)];
+            
+            if (node.left) [queue addObject:node.left];
+            if (node.right) [queue addObject:node.right];
+        }
+        [queue removeObjectsInRange:NSMakeRange(0, count)];
+        [result addObject:[nodes copy]];
+        
+        count = queue.count;
+    }
+    
+    return [result copy];
+}
+
+@end
+
+int main(int argc, char * argv[]) {
+    TNode *node3 = [[TNode alloc] initWithValue:3];
+    TNode *node9 = [[TNode alloc] initWithValue:9];
+    TNode *node20 = [[TNode alloc] initWithValue:20];
+    TNode *node15 = [[TNode alloc] initWithValue:15];
+    TNode *node7 = [[TNode alloc] initWithValue:7];
+    
+    node3.left = node9;
+    node3.right = node20;
+    
+    node20.left = node15;
+    node20.right = node7;
+    
+    NSLog(@"%@", [node3 levelOrderTraversal]);
+}
+
