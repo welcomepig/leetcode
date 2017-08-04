fe681981eb55c094db14bb74295681612824a629
Tina Chang <welcomepig@gmail.com>
2017-08-04 21:47:32 +0800

    Updated 098

--- 098-Validate-Binary-Search-Tree/098_Validate_Binary_Search_Tree2.m
+++ 098-Validate-Binary-Search-Tree/098_Validate_Binary_Search_Tree2.m
@@ -1,22 +1,26 @@
-#import <Foundation/Foundation.h>
-#import <stdio.h>
-
-@interface NSNode : NSObject
+@interface TNode : NSObject
 
 @property (nonatomic, assign) NSInteger value;
-@property (nonatomic, strong) NSNode *left;
-@property (nonatomic, strong) NSNode *right;
+@property (nonatomic, strong) TNode *left;
+@property (nonatomic, strong) TNode *right;
 
--(instancetype)initWithValue:(NSInteger)value;
--(void)insert:(NSInteger)value;
--(BOOL)isValidBST;
+- (instancetype)initWithValue:(NSInteger)value;
+- (void)insert:(NSInteger)value;
+- (BOOL)isValidBST;
++ (BOOL)isValidBST:(TNode*)node;
 
 @end
 
-@implementation NSNode
+@implementation TNode
 
--(instancetype)initWithValue:(NSInteger)value
-{
+- (NSString*)description {
+    /* inorder */
+    NSString *ldesc = self.left ? [NSString stringWithFormat:@"%@", self.left] : @"";
+    NSString *rdesc = self.right ? [NSString stringWithFormat:@"%@", self.right] : @"";
+    return [NSString stringWithFormat:@"%@ %ld %@", ldesc, (long)self.value, rdesc];
+}
+
+- (instancetype)initWithValue:(NSInteger)value {
     self = [super init];
     if (self) {
         _value = value;
@@ -26,69 +30,68 @@
     return self;
 }
 
--(void)insert:(NSInteger)value
-{
+- (void)insert:(NSInteger)value {
     if (value < self.value) {
-        if (self.left) {
-            [self.left insert:value];
+        if (!self.left) {
+            self.left = [[TNode alloc] initWithValue:value];
         } else {
-            self.left = [[NSNode alloc] initWithValue:value];
+            [self.left insert:value];
         }
     } else {
-        if (self.right) {
-            [self.right insert:value];
+        if (!self.right) {
+            self.right = [[TNode alloc] initWithValue:value];
         } else {
-            self.right = [[NSNode alloc] initWithValue:value];
+            [self.right insert:value];
         }
     }
 }
 
--(BOOL)isValidBST:(NSNumber*)preval
-{
-    if (self.left && ![self.left isValidBST:preval]) {
+#pragma mark - method (instance)
+
+- (BOOL)isValidBST:(NSNumber*)preValue {
+    if (self.left && ![self.left isValidBST:preValue]) {
         return NO;
     }
+    if ([preValue integerValue] >= self.value) return NO;
+    preValue = @(self.value);
     
-    if ([preval integerValue] >= self.value) {
-        return NO;
-    }
-    preval = @(self.value);
-    
-    if (self.right && ![self.right isValidBST:preval]) {
+    if (self.right && ![self.right isValidBST:preValue]) {
         return NO;
     }
     
     return YES;
 }
 
--(BOOL)isValidBST
-{
-    NSNumber *preval = @(NSIntegerMin);
-    return [self isValidBST:preval];
+- (BOOL)isValidBST {
+    return [self isValidBST:@(NSIntegerMin)];
+}
+
+#pragma mark - method (class)
+
++ (BOOL)isValidBST:(TNode*)node preValue:(NSNumber*)preValue {
+    if (!node) return YES;
+    if (![TNode isValidBST:node.left preValue:preValue]) return NO;
+    if ([preValue integerValue] >= node.value) return NO;
+    preValue = @(node.value);
+    
+    return [TNode isValidBST:node.right preValue:preValue];
+}
+
++ (BOOL)isValidBST:(TNode*)node {
+    return [TNode isValidBST:node preValue:@(NSIntegerMin)];
 }
 
 @end
 
-int main (int argc, const char * argv[])
-{
-    @autoreleasepool {
-        NSNode *tree = [[NSNode alloc] initWithValue:5];
-        [tree insert:3];
-        [tree insert:2];
-        [tree insert:4];
-        [tree insert:7];
-        [tree insert:6];
-        [tree insert:8];
-        
-        NSNode *tree2 = [[NSNode alloc] initWithValue:5];
-        [tree2 insert:3];
-        [tree2 insert:2];
-        [tree2 insert:4];
-        [tree2 insert:7];
-        [tree2 insert:6];
-        [tree2 insert:8];
-        
-        NSLog(@"%d", [tree isValidBST]);
-    }
+int main(int argc, char * argv[]) {
+    TNode *tree = [[TNode alloc] initWithValue:5];
+    [tree insert:3];
+    [tree insert:2];
+    [tree insert:4];
+    [tree insert:7];
+    [tree insert:6];
+    [tree insert:8];
+    
+    NSLog(@"%@ %d", tree, [tree isValidBST]);
 }
 
