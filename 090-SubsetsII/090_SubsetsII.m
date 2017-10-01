688dda5f14ee1976594ae7ad54054bc02d8e1726
Tina Chang <welcomepig@gmail.com>
2017-10-01 22:58:02 +0800

    Added 090

---
+++ 090-SubsetsII/090_SubsetsII.m
@@ -0,0 +1,39 @@
+#import <Foundation/Foundation.h>
+
+@interface NSArray (SubsetsII)
+
+- (NSArray*)subsets;
+
+@end
+
+@implementation NSArray (SubsetsII)
+
+- (NSArray*)subsets
+{
+    NSMutableArray *subsets = [NSMutableArray array];
+    NSInteger i, size, preSize = 0;
+    id preObj = nil;
+    
+    [subsets addObject:@[]];
+    for (id obj in self) {
+
+        size = subsets.count;
+        i = [obj isEqual:preObj] ? 0 : preSize; 
+        for (;i < size; i++) {
+            NSMutableArray *row = [subsets[i] mutableCopy];
+            [row addObject:obj];
+            [subsets addObject:row];
+        }
+        
+        preObj = obj;
+        preSize = size;
+    }
+    
+    return [subsets copy];
+}
+
+@end
+
+int main(int argc, char * argv[]) {
+    NSLog(@"%@", [@[@(1), @(2), @(2)] subsets]);
+}
